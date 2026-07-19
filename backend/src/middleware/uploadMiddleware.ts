47da9d6c3977ee4566fd0b6944c16a31acf2dac3
Jiya Agrawal <jiyaagrawal@Jiyas-MacBook-Air.local>
2026-07-19 11:25:03 +0530

    Fix upload middleware and deployment issues

--- backend/src/middleware/uploadMiddleware.ts
+++ backend/src/middleware/uploadMiddleware.ts
@@ -1,10 +1,9 @@
 import multer from "multer";
-import { storage } from "../config/cloudinary";
 
+const storage = multer.memoryStorage();
 
 const upload = multer({
   storage,
 });
-
 
 export default upload;
